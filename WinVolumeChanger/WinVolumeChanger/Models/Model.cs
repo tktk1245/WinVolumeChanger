01c3e210c709b6533ffe51881f1d4283a5830b90
tktk1245 <43270214+tktk1245@users.noreply.github.com>
2018-10-28 23:06:42 +0900

    1.0.0 新規作成
    Windows音量変更ツールを新規作成した

---
+++ WinVolumeChanger/WinVolumeChanger/Models/Model.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WinVolumeChanger.Models
+{
+
+    public class WinVolumeChangerModel : NotificationObject
+    {
+        #region 定数
+        private static int MAX_VOLUME = 100;
+        private static int MIN_VOLUME = 0;
+        #endregion
+
+        private CoreAudioDevice _AudioDevice;
+
+        #region CurrentVolume変更通知プロパティ
+        private int _CurrentVolume = 0;
+
+        public int CurrentVolume
+        {
+            get
+            { return _CurrentVolume; }
+            set
+            { 
+                if (_CurrentVolume == value)
+                    return;
+                _CurrentVolume = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        public WinVolumeChangerModel()
+        {
+            //コンストラクタ
+            try
+            {
+                _AudioDevice = new CoreAudioController().DefaultPlaybackDevice;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("DefaultPlaybackDevice Get Error:" + ex);
+            }
+        }
+
+        /// <summary>
+        /// 音量を上げる
+        /// </summary>
+        async public void UpVolume()
+        {
+            this.GetVolume();
+            int volumeValue = CurrentVolume == MAX_VOLUME ? CurrentVolume : ++CurrentVolume;
+            await _AudioDevice.SetVolumeAsync(volumeValue);
+
+            Debug.WriteLine("Set Volume:" + volumeValue);
+            this.GetVolume();
+
+        }
+
+        /// <summary>
+        /// 音量を下げる
+        /// </summary>
+        async public void DownVolume()
+        {
+            this.GetVolume();
+            int volumeValue = CurrentVolume == MIN_VOLUME ? CurrentVolume : --CurrentVolume;
+            await _AudioDevice.SetVolumeAsync(volumeValue);
+
+            Debug.WriteLine("Set Volume:" + volumeValue);
+            this.GetVolume();
+
+        }
+
+        /// <summary>
+        /// 音量を最大にする
+        /// </summary>
+        async public void SetVolumeMax()
+        {
+            await _AudioDevice.SetVolumeAsync(MAX_VOLUME);
+
+            Debug.WriteLine("Set Volume:" + MAX_VOLUME);
+            this.GetVolume();
+
+        }
+
+        /// <summary>
+        /// 音量を最小にする
+        /// </summary>
+        async public void SetVolumeMute()
+        {
+            await _AudioDevice.SetVolumeAsync(MIN_VOLUME);
+
+            Debug.WriteLine("Set Volume:" + MIN_VOLUME);
+            this.GetVolume();
+
+        }
+
+        /// <summary>
+        /// 現在音量の取得
+        /// </summary>
+        async public void GetVolume()
+        {
+            CurrentVolume = (int)await _AudioDevice.GetVolumeAsync();
+        }
+    }
+}
---
+++ WinVolumeChanger/WinVolumeChanger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using WinVolumeChanger.Models;
+
+namespace WinVolumeChanger.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private WinVolumeChangerModel _Model;
+        private PropertyChangedEventListener _ModelListener;
+
+        #region VolumeValue変更通知プロパティ
+        private string _CurrentValue = "0";
+
+        public string CurrentVolume
+        {
+            get
+            { return _CurrentValue; }
+            set
+            { 
+                if (_CurrentValue == value)
+                    return;
+                _CurrentValue = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        public void Initialize()
+        {
+            _Model = new WinVolumeChangerModel();
+
+            //Modelデータリスナの初期化
+            _ModelListener = new PropertyChangedEventListener(_Model) {
+              () => _Model.CurrentVolume, ((sender, e) => CurrentVolumePropertyChanged())
+            };
+
+            CompositeDisposable.Add(_ModelListener);
+
+            _Model.GetVolume();
+        }
+
+        /// <summary>
+        /// スクロール上の処理
+        /// </summary>
+        public void ScrollUp()
+        {
+            _Model.UpVolume();
+        }
+
+        /// <summary>
+        /// スクロール下の処理
+        /// </summary>
+        public void ScrollDown()
+        {
+            _Model.DownVolume();
+        }
+
+
+        /// <summary>
+        /// 音量ミュート処理
+        /// </summary>
+        public void VolumeMute()
+        {
+            _Model.SetVolumeMute();
+        }
+
+        /// <summary>
+        /// 音量MAX処理
+        /// </summary>
+        public void VolumeMax()
+        {
+            _Model.SetVolumeMax();
+        }
+
+        /// <summary>
+        /// 現在音量の更新
+        /// </summary>
+        public void CurrentVolumePropertyChanged()
+        {
+            this.CurrentVolume = _Model.CurrentVolume.ToString();
+        }
+    }
+}
